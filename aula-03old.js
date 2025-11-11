f381a61769b843f91386f987515030fedf59570e
mayki-douglas <mayki.douglas1@gmail.com>
2025-11-11 11:51:17 -0300

    arquivo JS no formado antigo do EcmaScript

---
+++ aula-03old.js
@@ -0,0 +1,12 @@
+var Curso = /** @class */ (function () {
+    function Curso() {
+        this.canal = null;
+        this.curso = null;
+    }
+    return Curso;
+}());
+this.canal = cancelAnimationFrame;
+this.curso = curso;
+var c1 = new Curso("CFB", "Typescript");
+console.log(c1.canal);
+console.log(c1.curso);
