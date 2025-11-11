a1570e4cd0abac302838b344eeafd63554b4c0be
mayki-douglas <mayki.douglas1@gmail.com>
2025-11-11 11:14:34 -0300

    Entendendo as formas de compilar TS pra JS

---
+++ src/aula-02.ts
@@ -0,0 +1,20 @@
+//entendendo as formas de compilar typescript para gerar o JavaScript
+
+const nome = "Mayki"
+const cpu = "AMD Ryzen"
+
+const novo = "hello world"
+
+console.log(nome);
+console.log(cpu);
+
+console.log(novo);
+
+
+//Typescript precisa ser compilado com um arquivo .js
+
+//ao executar um arquivo TS, usar o comando tsc "nome do arquivo" que ele irá gerar um arquivo .js
+
+//se rodar o comando node "nome do arquivo.ts" o arquivo roda, mas é obrigatório gerar um arquivo.js por o TS ser fortemente tipado
+
+//ao inserir informaçoes novas, é obrigatório rodar o comando novamente para que o arquivo .js insira as informações novas
